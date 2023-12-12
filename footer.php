d46035a38e5bdd97e99f7d2868b5e3aed3f4f29a
Avat632 <135575206+Avat632@users.noreply.github.com>
2023-12-12 12:18:15 +0700

    Add files via upload

---
+++ footer.php
@@ -0,0 +1,23 @@
+<section class="footer">
+
+    <div class="box-container">
+
+        <div class="box">
+            <h3>contact info</h3>
+            <p> <i class="fas fa-phone"></i> +211-0605-0016 </p>
+            <p> <i class="fas fa-envelope"></i> MuhammadAvat@gmail.com </p>
+            <p> <i class="fas fa-map-marker-alt"></i> Bantul, Yogyakarta - 537232 </p>
+        </div>
+
+        <div class="box">
+            <h3>follow us</h3>
+            <a href="#"><i class="fab fa-facebook-f"></i>facebook</a>
+            <a href="#"><i class="fab fa-twitter"></i>twitter</a>
+            <a href="#"><i class="fab fa-instagram"></i>instagram</a>
+        </div>
+
+    </div>
+
+    <div class="credit">This is the end. <a href="#">Go back up</a> </div>
+
+</section>
